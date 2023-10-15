044cd8726b8b1bed165cfef02e55cc9ed102aee5
Yeison Martinez <yeisonl2036@gmail.com>
2023-10-15 14:10:22 -0500

    Adding a new template

---
+++ app/Models/Clinic.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Clinic extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+       'clinic_number',
+        'user_id',
+        'status', 
+    ];
+
+   public function user() : BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function clinic_tower(): HasMany{
+        return $this->hasMany(ClinicTower::class);
+    }
+}
